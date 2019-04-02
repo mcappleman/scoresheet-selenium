ff5ede8f552b904c7d7ad394dee9f2aaf8dc4b83
Matthew Cappleman <matt.cappleman@gmail.com>
2019-04-02 13:00:27 -0400

    Downloading the csv successfully

---
+++ classes/exceptions.py
@@ -0,0 +1,4 @@
+
+class CSVDownloadError(Exception):
+    """Raised when Selenium cannot download the CSV File for use"""
+    pass
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import json
 import sys
+import time
+import os
 
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+from classes.exceptions import CSVDownloadError
 
 
 def main(argv):
@@ -15,7 +20,12 @@ def main(argv):
     env = load_env('environment.json')
 
     # Do Stuff Here
-    download_csv(driver, env)
+    try:
+        download_csv(driver, env)
+    except CSVDownloadError:
+        print('Unable to Download the CSV')
+    except:
+        print('Another error happened somewhere')
 
     driver.close()
 
@@ -25,6 +35,10 @@ def load_env(json_env_path):
     with open(json_env_path) as json_file:
         env = json.load(json_file)
 
+    csv_file = Path(env['DOWNLOAD_PATH'])
+    if csv_file.is_file():
+        os.remove(env['DOWNLOAD_PATH'])
+    
     return env
 
 
@@ -32,6 +46,15 @@ def download_csv(driver, env):
     driver.get(env['CSV_URL'])
     csv_elem = driver.find_element_by_xpath('/html/body/div/font/big/b/a[1]')
     csv_elem.click()
+    
+    csv_file = Path(env['DOWNLOAD_PATH'])
+    attempt = 0
+    max_attempt = 15
+    while not csv_file.is_file():
+        if attempt > max_attempt:
+            raise CSVDownloadError()
+        time.sleep(10)
+        attempt += 1
 
 
 if __name__ == "__main__":
